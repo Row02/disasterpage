cd58373779cc0ef9dfa60b4d395ea152ffdf4425
ranielletuazon <radtuazon.student@ua.edu.ph>
2024-11-21 23:54:14 +0800

    Update November 21

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -74,17 +74,64 @@ export default function Homepage(){
                 </div>
                 <div className={styles.sixthContent}>
                     <div className={styles.leftCardSix}>
-                        <div className={styles.sixthHeader} style={{fontSize:"3.5rem", color:"black", fontWeight:"bold"}}>Santa Ana Community Alert</div>
-                        <div className={styles.miniHeader} style={{fontSize:"1.2rem", fontWeight:"bold"}}>Stay informed, Stay safe</div>
-                        <div className={styles.sixthContext} style={{fontSize:"1rem", color:"#374151"}}>Join us in our mission to ensure the safety and preparedness of every Santa Ana
+                        <div className={styles.sixthHeader} style={{fontSize:"3.5rem", color:"#0f172a", fontWeight:"bolder", lineHeight:"1", marginBottom:"1rem"}}>Santa Ana Community Alert</div>
+                        <div className={styles.miniHeader} style={{fontSize:"1.2rem", fontWeight:"bold", color:"#0f172a", marginBottom:"1rem"}}>Stay informed, Stay safe</div>
+                        <div className={styles.sixthContext} style={{fontSize:"1rem", color:"#0f172a", marginBottom:"2rem"}}>Join us in our mission to ensure the safety and preparedness of every Santa Ana
                             resident. Discover the latest disaster news and essential updates right here.
                         </div>
-                        <div className={styles.sixthButtons}>
+                        <div className={styles.sixthButtons} style={{marginBottom:"2rem"}}>
                             <div className={styles.preparednessButton}>Get Preparedness Tips</div>
-                            <div className={styles.preparednessButton}>Explore MDRRMO Resources</div>
+                            <div className={styles.exploreMDRRMOButton}>Explore MDRRMO Resources</div>
+                        </div>
+                        <div className={styles.icons}>
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-instagram"></i>
                         </div>
                     </div>
                     <div className={styles.rightCardSix}></div>
+                </div>
+                <div className={styles.seventhContent}>
+                    <div className={styles.aboveContent}>
+                        <div className={styles.aboveLeft}>
+                            <div className={styles.seventhHeader} style={{fontSize:"4rem", color:"#0f172a", fontWeight:"bold"}}>Contact the MDRRMO</div>
+                            <div className={styles.seventhText} style={{fontSize:"1rem", color:"#0f172a"}}>Reach out to us for quick reporting and inquiries. We're here to help 24/7.</div>
+                        </div>
+                        <div className={styles.aboveRight}></div>
+                    </div>
+                    <div className={styles.belowContent}>
+                        <div className={styles.sections}>
+                            <div className={styles.secTitle}>SANTA ANA OFFICE</div>
+                            <div className={styles.secText}>Monday-Sunday 24/7</div>
+                        </div>
+                        <div className={styles.sections}>
+                            <div className={styles.secTitle}>Our Address</div>
+                            <div className={styles.secText}>Santa Ana, Pampanga</div>
+                        </div>
+                        <div className={styles.sections}>
+                            <div className={styles.secTitle}>Get in touch</div>
+                            <div className={styles.secText}>+63 912 3456 789</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.footer}>
+                    <div className={styles.aboveFooter}>
+                        <div className={styles.navLinks}>
+                            <span>Municipal Disaster Hub</span>
+                            <span>Emergency Help</span>
+                            <span>MDRRMO Resources</span>
+                            <span>Contact Us</span>
+                        </div>
+                    </div>
+                    <div className={styles.belowFooter}>
+                        <div className={styles.iconLinks}>
+                            <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-facebook"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                            <i class="fa-brands fa-reddit"></i>
+                        </div>
+                        <div className={styles.allRights}>© 2023 Municipal Disaster Hub, Santa Ana. All Rights Reserved.</div>
+                    </div>
                 </div>
             </div>
         </>
